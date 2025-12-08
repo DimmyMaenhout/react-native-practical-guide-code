7d3dcc23b54fede08c2924044c5e1d771b2f5510
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2025-12-08 20:03:36 +0100

    section-6: Completed until point 101 (Adding Images & Styling) - Added FlatList to MealsOverViewScreen to display all the different meals for a category - Added MealItem component & set layout and styling including image from the internet

--- attachments/06-navigation/00-starting-project/screens/MealsOverviewScreen.js
+++ attachments/06-navigation/00-starting-project/screens/MealsOverviewScreen.js
@@ -1,5 +1,7 @@
 import { View, Text, StyleSheet } from 'react-native';
 import { MEALS } from '../data/dummy-data';
+import { FlatList } from 'react-native';
+import MealItem from '../components/MealItem';
 // We kunnen ook een hook gebruiken ipv de via de properties
 // import { useRoute } from '@react-navigation/native';
 
@@ -7,9 +9,30 @@ import { MEALS } from '../data/dummy-data';
 function MealsOverviewScreen({ route }) {
   const catId = route.params.categoryId;
 
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: itemData.item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration
+    };
+    return <MealItem {...mealItemProps} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
