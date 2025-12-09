51d354aa202b67c3b301221e2bc54f206e4037e3
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2025-12-09 10:29:50 +0100

    section-6: Completed point 111 - Added a bottom tab bar

--- attachments/06-navigation/11-other-navigators-starting-code/App.js
+++ attachments/06-navigation/11-other-navigators-starting-code/App.js
@@ -1,44 +1,77 @@
-import { createDrawerNavigator } from '@react-navigation/drawer';
+// import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import WelcomeScreen from './screens/WelcomeScreen';
 import UserScreen from './screens/UserScreen';
 import { Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Drawer = createDrawerNavigator();
+// const Drawer = createDrawerNavigator();
+
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
   return (
+    // NAVIGATION USING DRAWER
+    // <NavigationContainer>
+    //   {/* Via the initialRouteName="User" param we can set the screen that should be shown initially instead of it using the order from top to bottom as we define it eg below */}
+    //   <Drawer.Navigator
+    //     screenOptions={{
+    //       headerStyle: { backgroundColor: '#3c0a6b' },
+    //       headerTintColor: 'white',
+    //       drawerActiveBackgroundColor: '#f0e1ff',
+    //       drawerActiveTintColor: '#3c0a6b'
+    //       // drawerStyle: { backgroundColor: '#ccc' }
+    //     }}
+    //   >
+    //     <Drawer.Screen
+    //       name='Welcome'
+    //       component={WelcomeScreen}
+    //       options={{
+    //         drawerLabel: 'Welcome Screen',
+    //         drawerIcon: ({ color, size }) => (
+    //           <Ionicons name='home' color={color} size={size} />
+    //         )
+    //       }}
+    //     />
+    //     <Drawer.Screen
+    //       name='User'
+    //       component={UserScreen}
+    //       options={{
+    //         drawerIcon: ({ color, size }) => (
+    //           <Ionicons name='person' size={size} />
+    //         )
+    //       }}
+    //     />
+    //   </Drawer.Navigator>
+    // </NavigationContainer>
+
     <NavigationContainer>
-      {/* Via the initialRouteName="User" param we can set the screen that should be shown initially instead of it using the order from top to bottom as we define it eg below */}
-      <Drawer.Navigator
+      <BottomTab.Navigator
         screenOptions={{
           headerStyle: { backgroundColor: '#3c0a6b' },
           headerTintColor: 'white',
-          drawerActiveBackgroundColor: '#f0e1ff',
-          drawerActiveTintColor: '#3c0a6b'
-          // drawerStyle: { backgroundColor: '#ccc' }
+          tabBarActiveTintColor: '#3c0a6b'
         }}
       >
-        <Drawer.Screen
+        <BottomTab.Screen
           name='Welcome'
           component={WelcomeScreen}
           options={{
-            drawerLabel: 'Welcome Screen',
-            drawerIcon: ({ color, size }) => (
+            tabBarIcon: ({ color, size }) => (
               <Ionicons name='home' color={color} size={size} />
             )
           }}
         />
-        <Drawer.Screen
+        <BottomTab.Screen
           name='User'
           component={UserScreen}
           options={{
-            drawerIcon: ({ color, size }) => (
-              <Ionicons name='person' size={size} />
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name='person' color={color} size={size} />
             )
           }}
         />
-      </Drawer.Navigator>
+      </BottomTab.Navigator>
     </NavigationContainer>
   );
 }
