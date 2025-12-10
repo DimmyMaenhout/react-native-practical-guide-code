4c5eff65cdb96c97b724918b638d88bb7315b94c
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2025-12-10 11:59:43 +0100

    section-8: completed to point 127 (Adding navigation with React Navigation & configuring Navigation) - Installed navigation packages (npm install @react-navigation/native & expo install react-native-screens react-native-safe-area-context & npm install @react-navigation/native-stack @react-navigation/botttom-tabs - Setup navigation with Stack & BottomTab

--- attachments/08-practice-app/00-starting-project/App.js
+++ attachments/08-practice-app/00-starting-project/App.js
@@ -1,20 +1,91 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+import AllExpenses from './screens/AllExpenses';
+import RecentExpenses from './screens/RecentExpenses';
+import ManageExpenses from './screens/ManageExpense';
+
+const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
+
+function StackNavigator() {
+  <Stack.Navigator>
+    <Stack.Screen
+      name='recentExpenses'
+      component={RecentExpenses}
+      options={{
+        headerRight: () => {
+          <Ionicons name='add' color='white' size={24} />;
+        }
+      }}
+    />
+  </Stack.Navigator>;
+}
+
+function ExpensesOverview() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BottomTab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: '#EBBD1E',
+        tabBarStyle: {
+          backgroundColor: '#1434A4'
+        },
+        headerRight: () => {
+          <Ionicons name='add' color='white' size={24} />;
+        }
+      }}
+    >
+      <BottomTab.Screen
+        name='RecentExpenses'
+        component={RecentExpenses}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='hourglass-outline' color={color} size={size} />
+          ),
+          title: 'Recent Expenses'
+        }}
+      />
+      <BottomTab.Screen
+        name='AllExpenses'
+        component={AllExpenses}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='calendar' color={color} size={24} />
+          ),
+          title: 'All Expenses'
+        }}
+      />
+    </BottomTab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <>
+      <StatusBar style='auto' />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            // name='recentExpenses'
+            // component={RecentExpenses}
+            // options={{
+            //   headerTintColor: 'white',
+            //   headerStyle: {
+            //     backgroundColor: '#1434A4'
+            //   },
+            //   headerRight: () => {
+            //     <Ionicons name='add' color='white' size={24} />;
+            //   }
+            // }}
+            name='ExpensesOverview'
+            component={ExpensesOverview}
+          />
+          <Stack.Screen name='MangeExpenses' component={ManageExpenses} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
