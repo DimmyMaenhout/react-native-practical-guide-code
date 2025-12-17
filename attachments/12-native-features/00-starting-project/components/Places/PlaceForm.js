99cca1960d578d6c2058cb3d04b9b6477dee9c4e
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2025-12-17 18:28:00 +0100

    section-12: Completed to point 214 (Styling place items) - Implemented the savePlaceHandler in PlafeForm.js - Added the getAddress utility method to convert coordinates to an addresss (reverse geocoding) - handleLocation (with useEffect hook) in the LocationPicker component - Updated the Place model - Implemented the AddPlace screen and navigate to the AllPlaces screen with the new place - Implemented the AllPlaces screen and check if there are places when the screen becomes visible or isFocused changed - Added Styling to the PlaceItem

--- attachments/12-native-features/00-starting-project/components/Places/PlaceForm.js
+++ attachments/12-native-features/00-starting-project/components/Places/PlaceForm.js
@@ -4,8 +4,9 @@ import { Colors } from '../../constants/colors';
 import ImagePicker from './ImagePicker';
 import LocationPicker from './LocationPicker';
 import Button from '../UI/Button';
+import { Place } from '../../Models/place';
 
-function PlaceForm() {
+function PlaceForm({ onCreatePlace }) {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [pickedLocation, setPickedLocation] = useState();
   const [selectImage, setSelectedImage] = useState();
@@ -25,9 +26,9 @@ function PlaceForm() {
   }, []);
 
   function savePlaceHandler() {
-    console.log(enteredTitle);
-    console.log(selectImage);
-    console.log(pickedLocation);
+    const placeData = new Place(enteredTitle, selectImage, pickedLocation);
+
+    onCreatePlace(placeData);
   }
 
   return (
