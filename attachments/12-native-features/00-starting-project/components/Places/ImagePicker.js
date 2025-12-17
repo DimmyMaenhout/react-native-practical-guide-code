0cec2c56c4ff02621e7e20e94fe171760941e7e4
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2025-12-17 14:44:14 +0100

    section-12: Completed to point 200 (Creating a custom button) - Created a OutlineButton component - Show image preview if a photo has been taken & use our OutlineButton component instead of the default button

--- attachments/12-native-features/00-starting-project/components/Places/ImagePicker.js
+++ attachments/12-native-features/00-starting-project/components/Places/ImagePicker.js
@@ -1,11 +1,15 @@
-import { Alert, Button, View } from 'react-native';
+import { Alert, Image, StyleSheet, Text, View } from 'react-native';
 import {
   launchCameraAsync,
   useCameraPermissions,
   PermissionStatus
 } from 'expo-image-picker';
+import { useState } from 'react';
+import { Colors } from '../../constants/colors';
+import OutlineButton from '../UI/OutlineButton';
 
 function ImagePicker() {
+  const [pickedImage, setPickedImage] = useState();
   const [cameraPermissionInformation, requestPermission] =
     useCameraPermissions();
 
@@ -40,15 +44,39 @@ function ImagePicker() {
       aspect: [16, 9],
       quality: 0.5
     });
-    console.log(image);
+
+    setPickedImage(image.uri);
+  }
+
+  let imagePreview = <Text>No image taken yet.</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
   }
 
   return (
     <View>
-      <View></View>
-      <Button title='Take Image' onPress={takeImageHandler} />
+      <View style={styles / imagePreview}>{imagePreview}</View>
+
+      <OutlineButton icon='camera'>Take Image</OutlineButton>
     </View>
   );
 }
 
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: '100%',
+    height: 200,
+    marginVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary100,
+    borderRadius: 4
+  },
+  image: {
+    width: '100%',
+    height: '100%'
+  }
+});
